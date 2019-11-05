faf01e2465cbcf3641287ad827cf9666fb6e5eb7
Patrick Wu <wotingwu@live.com>
2019-11-05 15:33:35 +0800

    update __init__

--- wslpy/__init__.py
+++ wslpy/__init__.py
@@ -1,13 +1,4 @@
-from pathlib import Path
-
-
-def __isWSL__():
-    try:
-        Path('/proc/sys/fs/binfmt_misc/WSLInterop').resolve()
-    except FileNotFoundError:
-        return False
-    else:
-        return True
+import os.path
 
 def isWSL():
     """
@@ -17,4 +8,6 @@ def isWSL():
     -------
     A boolean value, `True` if it is WSL.
     """
-    return __isWSL__()
+    return os.path.exists('/proc/sys/fs/binfmt_misc/WSLInterop')
+
+print(isWSL())
