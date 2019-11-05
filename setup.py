cb456e1cd75343c8d3806c2c91ad8c777bd5c19d
Patrick Wu <wotingwu@live.com>
2019-11-05 15:37:50 +0800

    update package

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
-from sphinx.setup_command import BuildDoc
-cmdclass = {'build_sphinx': BuildDoc}
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='wslpy',
-    version='0.2.0',
+    version='0.8.1',
     description='Python Library for WSL specific tasks',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -22,12 +20,5 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3 :: Only"
-    ],
-    cmdclass=cmdclass,
-    command_options={
-        'build_sphinx': {
-            'project': ('setup.py', 'wslpy'),
-            'version': ('setup.py', '0.2'),
-            'release': ('setup.py', '0.2.0'),
-            'source_dir': ('setup.py', 'docsources')}},
+    ]
 )
