250893bebd637878398a67394f9eef6eb5b6775d
shubhsboson <41443146+shubhsboson@users.noreply.github.com>
2020-11-06 07:17:30 +0530

    changes as requested

--- wslpy/exec.py
+++ wslpy/exec.py
@@ -22,22 +22,22 @@ def isInteropEnabled():
     _______
     A boolean value, `True` if interoperablility is enabled.
     """
-    if __isInteropEnabled__() == 'enabled':
-        return True
-    else:
-        return False
+    return __isInteropEnabled__() == 'enabled'
 
 
 def preCheck():
 
     if isWSL():
-        if isInteropEnabled():
-            pass
-        else:
+        if not isInteropEnabled():
             sys.exit(
                 "Please enable interop /n Use 'echo 1 > /proc/sys/fs/binfmt_misc/WSLInterop'")
     else:
         sys.exit("This is not a wsl distribution")
+
+def preCheckAssert():
+
+    assert isWSL(),"This is not a wsl distribution"
+    assert isInteropEnabled(),"Please enable interop /n Use 'echo 1 > /proc/sys/fs/binfmt_misc/WSLInterop'"
 
 
 def cmd(command):
@@ -49,7 +49,8 @@ def cmd(command):
     command : str
         string of `cmd.exe` commands.
     """
-    preCheck()
+    #preCheck()
+    preCheckAssert()
     cmd = u"cmd.exe /c \""+command+u"\""
     subprocess.call(cmd, shell=True)
 
