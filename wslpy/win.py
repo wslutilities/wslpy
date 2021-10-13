f0e200fe3a64576f21d9738e1bfa232088eb9451
Jinming Wu, Patrick <me@patrickwu.space>
2021-10-14 02:23:57 +0800

    get display scaling (closes #21)

--- wslpy/win.py
+++ wslpy/win.py
@@ -1,5 +1,6 @@
 from wslpy.core.check import get_mount_prefix, get_sys_drive_prefix
 from wslpy.core.access import distro_info, registry
+from wslpy.exec import winps
 
 
 def get_current_execuable():
@@ -22,6 +23,47 @@ def get_current_execuable():
         return os.listdir(exe_real_loc)[0]
     else:
         return None
+
+
+def get_display_scaling():
+    """
+    Get Windows Display Scaling
+    """
+    command = """
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+public class DPI {
+    [DllImport("gdi32.dll")]
+    static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+
+    public enum DeviceCap {
+    VERTRES = 10,
+    DESKTOPVERTRES = 117
+    }
+
+    public static float scaling() {
+    Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+    IntPtr desktop = g.GetHdc();
+    int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+    int PhysicalScreenHeight = GetDeviceCaps(desktop,
+        (int)DeviceCap.DESKTOPVERTRES);
+
+    return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+    }
+}
+'@ -ReferencedAssemblies 'System.Drawing.dll'
+
+[Math]::round([DPI]::scaling(), 2) * 100
+"""
+
+    p = winps(command)
+    if p.returncode:
+        raise Exception("Failed to get display scaling")
+    dscale = int(p.stdout.rstrip()) / 100
+    return dscale
 
 
 __all__ = [
